aac3d2c8c56a835417c779148be3e91482ec96fa
zicopan <psxbro@hotmail.com>
2020-04-09 15:31:14 -0400

    add working background change, clear and hide menu

--- PlacesVR/index.js
+++ PlacesVR/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   AppRegistry,
   StyleSheet,
@@ -7,12 +7,13 @@ import {
   VrButton,
   Environment,
   asset,
-} from 'react-360';
+} from "react-360";
 
 export default class Hello360 extends React.Component {
-// Our component will keep track of this state
+  // Our component will keep track of this state
   state = {
     // count: 0,
+    menuHidden: false,
     currentBackground: "360_world.jpg",
   };
 
@@ -20,31 +21,75 @@ export default class Hello360 extends React.Component {
   // incrementCount = () => {
   //   this.setState({count: this.state.count + 1});
   // };
-  
+
+  hideMenu = () => {
+    this.setState({ menuHidden: true });
+  };
+
+  showMenu = () => {
+    this.setState({ menuHidden: false });
+  };
+
   changeBackground() {
     // Set the background to a 360 or 180 image
     Environment.setBackgroundImage(
       asset("calg.jpg"),
-      {format: '2D'}, /* one of the formats mentioned above */
+      { format: "2D" } /* one of the formats mentioned above */
+    );
+  }
+
+  clearBackground() {
+    Environment.setBackgroundImage(
+      asset("360_world.jpg"),
+      { format: "2D" } /* one of the formats mentioned above */
     );
   }
 
   render() {
-    return (
-      <View //style={styles.panel}>
-      >
-        <VrButton
-          // onClick={this.incrementCount}
-          onClick={this.changeBackground}
-          style={styles.greetingBox}>
-          <Text style={styles.greeting}>
-            Kananaskis
-          </Text>
-        </VrButton>
-      </View>
-    );
+    if (!this.state.menuHidden) {
+      return (
+        <View style={styles.panel}>
+          <View style={styles.hideMenuPanel}>
+            <VrButton onClick={this.hideMenu} style={styles.hideMenuBox}>
+              <Text style={styles.greeting}>Hide Menu</Text>
+            </VrButton>
+          </View>
+          <View style={styles.placesPanel}>
+            <VrButton
+              onClick={this.changeBackground}
+              style={styles.greetingBox}
+            >
+              <Text style={styles.greeting}>Kananaskis</Text>
+            </VrButton>
+            <VrButton
+              onClick={this.changeBackground}
+              style={styles.greetingBox}
+            >
+              <Text style={styles.greeting}>Kananaskis</Text>
+            </VrButton>
+            <VrButton
+              onClick={this.clearBackground}
+              style={styles.clearBox}
+            >
+              <Text style={styles.clearText}>Clear</Text>
+            </VrButton>
+          </View>
+        </View>
+      );
+    } else {
+      return (
+        <View //style={styles.panel}>
+        >
+          <VrButton
+            onClick={this.showMenu}
+            style={styles.showMenuBox}
+          >
+            <Text style={{color: "white"}}>Show Menu</Text>
+          </VrButton>
+        </View>
+      );
+    }
   }
-
 }
 
 const styles = StyleSheet.create({
@@ -52,19 +97,47 @@ const styles = StyleSheet.create({
     // Fill the entire surface
     width: 1000,
     height: 600,
-    backgroundColor: 'rgba(255, 255, 255, 0.4)',
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: "rgba(255, 255, 255, 0.4)",
+  },
+  clearText: {
+    color: "black",
+    fontSize: 30,
+  },
+  clearBox: {
+    padding: 20,
+    backgroundColor: "#ffffff",
+    borderColor: "#ff0000",
+    borderWidth: 2,
+  },
+  placesPanel: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
   },
   greetingBox: {
     padding: 20,
-    backgroundColor: '#000000',
-    borderColor: '#639dda',
+    backgroundColor: "#000000",
+    borderColor: "#639dda",
     borderWidth: 2,
   },
   greeting: {
     fontSize: 30,
   },
+  hideMenuPanel: {},
+  hideMenuBox: {
+    padding: 1,
+    marginRight: '18px',
+    width: '14px',
+    backgroundColor: "#000000",
+    borderColor: "#639dda",
+    borderWidth: 2,
+  },
+  showMenuBox : {
+    width: '100px',
+    padding: 2,
+    backgroundColor: "rgba(255, 255, 255, 0.4)",
+  }
 });
 
-AppRegistry.registerComponent('Hello360', () => Hello360);
+AppRegistry.registerComponent("Hello360", () => Hello360);
