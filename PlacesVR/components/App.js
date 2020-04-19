9dbde201b8c87ff5d90f10030d0422c57a780770
zicopan <psxbro@hotmail.com>
2020-04-18 21:58:15 -0400

    add working login with auth to show landing

--- PlacesVR/components/App.js
+++ PlacesVR/components/App.js
@@ -8,16 +8,59 @@ import {
   asset,
 } from "react-360";
 
-// firebase stuff goes in here (to determine if authenticated)
-
+import fire from "../Firebase";
 import Landing from "./Landing/Landing";
 import SignIn from "./SignIn/SignIn";
 
 export default class App extends React.Component {
-  render() {
-    return (
-      //<Landing />
-      <SignIn /> 
-    );
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: null,
+    };
   }
-};
+
+  componentDidMount() {
+    this.authListener();
+  }
+
+  /**
+   * Sets the user state to the current logged in user (either from cookies or login)
+   * via SignIn.js inputs
+   */
+  authListener() {
+    fire.auth().onAuthStateChanged((user) => {
+      console.log("Current User: ", user);
+      if (user) {
+        this.setState({ user: user });
+      } else {
+        this.setState({ user: null });
+      }
+    });
+  }
+
+  render() {
+    if (this.state.user) {
+      return <Landing />;
+    } else {
+      return (
+        <SignIn />
+      );
+    }
+  }
+}
+
+/**
+ * Allows for App state to be manipulated by child components
+ * need this for when uploading a photo
+ * - upload photo to db --> update state in App which re-renders landing
+ * with the update props (containing new data from the db)
+ */
+// updateAppState = ({ compiling, landingHandlers, pdfPath, latexPath }) => {
+//   this.setState({
+//     ...(pdfPath != undefined && { pdfPath }),
+//     ...(latexPath != undefined && { latexPath }),
+//     ...(compiling != undefined && { compiling }),
+//     ...(landingHandlers != undefined && { landingHandlers }),
+//   });
+// };
